d5dfe038c0d13e0b88a15b6eb4acdd808cfb4857
Scott Hallock <scott@hallocks.net>
2023-02-23 22:02:09 +0000

    End of Lesson 1
    Canvas with sizing, asset imagess

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+
+window.addEventListener("load", () => {
+  const canvas = document.querySelector("#canvas1");
+  const ctx = canvas.getContext("2d");
+  canvas.width = 1280;
+  canvas.height = 720;
+});
