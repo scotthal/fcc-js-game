b82bd194746d8ee38abb93b873316e4af743f6e8
Scott Hallock <scott@hallocks.net>
2023-02-26 00:52:14 +0000

    End of Lesson 2

--- app.js
+++ app.js
@@ -5,4 +5,24 @@ window.addEventListener("load", () => {
   const ctx = canvas.getContext("2d");
   canvas.width = 1280;
   canvas.height = 720;
+
+  class Player {
+    constructor(game) {
+      this.game = game;
+    }
+  }
+
+  class Game {
+    constructor(canvas) {
+      this.canvas = canvas;
+      this.width = this.canvas.width;
+      this.height = this.canvas.height;
+      this.player = new Player(this);
+    }
+  }
+
+  const game = new Game(canvas);
+  console.log(game);
+
+  const animate = () => {};
 });
